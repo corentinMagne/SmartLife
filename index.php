a0e98b162d8f0141e2ac0f8b9a6b2ec20705a617
Antoine Mille <antoine.mille.site@gmail.com>
2016-11-19 11:58:53 +0100

    Add connexion form

--- index.php
+++ index.php
@@ -15,3 +15,50 @@ while ($donnees = $req->fetch())
 $req->closeCursor();
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>SmartLife</title>
+    </head>
+    
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <article>
+            	<?php
+				if (isset($_POST['email']) AND isset($_POST['pass'])) {
+            	?>
+            	yes<br>
+            	<?php
+            	} else {
+            	?>
+            	yes1<br>
+            	<?php
+            	}
+            	?>
+            	<p>
+					<form action="#" method="post">
+						<p>
+    						<input type="text" id="email" name="email" />
+    						<input type="password" id="pass" name="pass" />
+    						<input type="submit" value="Connexion" />
+						</p>
+					</form>
+            	</p>
+            </article>
+        </section>       
+        <footer>
+        </footer>
+    </body>
+</html>
