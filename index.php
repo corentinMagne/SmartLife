6ea87187f5601547373d0486596f3b234857ae55
Antoine Mille <antoine.mille.site@gmail.com>
2016-11-19 11:36:06 +0100

    Add connexion to database

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-Coucou
+<?php require('includes/config.php'); ?>
+
+<?php
+$req = $bdd->prepare('SELECT * FROM users');
+$req->execute();
+while ($donnees = $req->fetch())
+{
+?>
+    <p>
+    	<?php echo $donnees['phone']; ?>
+   </p>
+<?php
+}
+
+$req->closeCursor();
+
+?>
